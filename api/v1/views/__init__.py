f453ffe82c2b01c74acf4e63feaa33d0e13c84ff
Zak-devz <zakariarassili@gmail.com>
2023-10-29 22:03:08 +0100

    fix import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""sharing app_views Blueprint"""
+"""app_views Blueprint"""
 
 from flask import Blueprint
-from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__)
+from api.v1.views.states import *
+from api.v1.views.index import *
