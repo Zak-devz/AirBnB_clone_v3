2af7514283b9f72ce478e77f819d1ae66e9ccd1f
Zak-devz <zakariarassili@gmail.com>
2023-10-29 21:54:54 +0100

    add exept

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,7 +68,6 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
 
     def get(self, cls, id):
         """Method to return the object based on the class and its ID"""
@@ -87,4 +86,3 @@ class FileStorage:
         else:
             count = len(self.all())
         return count
-
