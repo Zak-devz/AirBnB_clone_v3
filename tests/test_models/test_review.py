6bd3a5a5f46394baaf5f0d588b7c446cafda02a6
root <ashraf@rassili.com>
2023-10-30 06:29:22 -0700

    task 1 correction 2

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -102,7 +102,11 @@ class TestReview(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in r.__dict__:
+<<<<<<< Updated upstream
             if attr != "_sa_instance_state":
+=======
+            if attr is != "_sa_instance_state":
+>>>>>>> Stashed changes
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
